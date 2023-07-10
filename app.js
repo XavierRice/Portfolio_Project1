3c76e10d2c1bc9cbec14097f4ba9e0a97358f1fb
xavierrice <xavier.rice@gmail.com>
2023-07-10 17:22:46 -0400

    added my landing page, nav bar and carousel

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const test = console.log
+const test = console.log //i hate typing out console.log too many letters and I am my own developemnt team
 
-    const options = {
-        method: 'GET',
+    const options = {     // this api requires fullfilment of the options and this seems to be a cleaner method of doing it.
+        method: 'GET',    
         headers: {
             'X-RapidAPI-Key': '95e39c8565mshd52d9d5c85d8b80p174bd5jsn5b0ef354524f',
             'X-RapidAPI-Host': 'horoscope34.p.rapidapi.com'
         }
     };
 
-fetch('https://horoscope34.p.rapidapi.com/api/horoscope/today', options)
+fetch('https://horoscope34.p.rapidapi.com/api/horoscope/signs', options)
     .then((response) => response.json())
     .then((JSONresponse) => {
-        const results = JSONresponse
-        test(results.payload)
+        const results = JSONresponse.payload
+        const signs = results.signs
+       test(results.images)
     })
     .catch((err) => test(err))
 
