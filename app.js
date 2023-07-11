dd7801379e0198862efa671e380a202628f9d717
xavierrice <xavier.rice@gmail.com>
2023-07-11 18:55:36 -0400

    trying to clean up the style n layout.

--- app.js
+++ app.js
@@ -43,22 +43,71 @@ fetch('https://horoscope34.p.rapidapi.com/api/horoscope/signs', options)
         }
     }
 
+    const errFlag = document.getElementById("errFlag")
+    const resultsCol = document.getElementById("results")
+   
 
 
     const form = document.querySelector("form")
+
     form.addEventListener("submit", (event) => {
         event.preventDefault();
 
         let userSign = event.target["signs"].value
         let userDob = event.target["date"].value
-        
+
+        if(userSign === "???"){
+            errFlag.innerHTML = "<h4><Strong>Must Enter Sign</Strong></h4>"
+            errFlag.style.color = "orange"
+            form.reset()
+            setTimeout(errFlag.reset, 2000)
+        }
+
+        fetch('https://horoscope34.p.rapidapi.com/api/horoscope/today', options1)
+        .then((response) => response.json())
+        .then((JSONresponse) =>{
+            const results = JSONresponse.payload 
+            test(results[`${userSign}`])
+            resultsCard(results[`${userSign}`])
+ 
+        })
+        .catch((err) => test(err))
     })
 
 
-    fetch('https://horoscope34.p.rapidapi.com/api/horoscope/today', options1)
-    .then((response) => response.json())
-    .then((JSONresponse) =>{
-        const results = JSONresponse.payload //
-        test(results)
+    function resultsCard(string){
 
-    })
+        const resultCard = document.createElement("article")
+        resultCard.classList.add("card")
+
+        const h3Title = document.createElement("h3")
+        h3Title.classList.add("HoroLabel")
+        h3Title.innerText = "Your Daily Horoscope"
+
+        const pHoroscope = document.createElement("p")
+        pHoroscope.innerHTML = `${string}`
+
+        resultCard.append(pHoroscope)
+        resultCard.appendChild(h3Title)
+        resultsCol.append(resultCard)
+
+        const advice = document.getElementById("advice")
+
+        fetch("https://api.adviceslip.com/advice")
+        .then((response) => response.json())
+        .then((JSONresponse) => {
+            const results = JSONresponse
+             test(results.slip.advice)
+
+        const h6Advice = document.createElement("h6")
+            h6Advice.innerHTML = results.slip.advice
+            h6Advice.style.textAlign="center"
+            h6Advice.classList.add("fs-4")
+            h6Advice.style.color="red"
+            advice.append(h6Advice)
+        })
+    
+}
+
+const images = document.getElementsByClassName("signs")
+
