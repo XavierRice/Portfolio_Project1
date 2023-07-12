a6a4e1e5c4cfc9b3bb60e530c727db2212a57752
xavierrice <xavier.rice@gmail.com>
2023-07-12 11:29:23 -0400

    got the hover figured out and added a read me

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const test = console.log //i hate typing out console.log too many letters and I am my own developemnt team
 
     const options = {     // this api requires fullfilment of the options and this seems to be a cleaner method of doing it.
-        method: 'GET',    
+        method: 'GET',    // These are one of the 5 http verbs GET, POST, PUT, DELETE and PATCH
         headers: {
-            'X-RapidAPI-Key': '95e39c8565mshd52d9d5c85d8b80p174bd5jsn5b0ef354524f',
+            'X-RapidAPI-Key': '95e39c8565mshd52d9d5c85d8b80p174bd5jsn5b0ef354524f',  // these are headers req. by Rapid API
             'X-RapidAPI-Host': 'horoscope34.p.rapidapi.com'
         }
     };
 
 fetch('https://horoscope34.p.rapidapi.com/api/horoscope/signs', options)
-    .then((response) => response.json())
+    .then((response) => response.json())            // This has to do with a "promise" made by the fetch statement and formatting that into JSON data.
     .then((JSONresponse) => {
-        const results = JSONresponse.payload
+        const results = JSONresponse.payload         // The returned results which is now an object/
         const signs = results.signs
         const images = results.images
 
-        const col2 = document.getElementById("sign_image")
-        const select = document.querySelector("select")
+        const col2 = document.getElementById("sign_image")    //using the DOM to access my col
+        const select = document.querySelector("select")       // and my select
         
-        for(let sign of signs){
+        for(let sign of signs){                                 //looping through my images gathered in my fetch
             const option = document.createElement("option")
             option.classList.add("options")
 
@@ -28,14 +28,14 @@ fetch('https://horoscope34.p.rapidapi.com/api/horoscope/signs', options)
             sign_image.setAttribute("src", picture)
             col2.appendChild(sign_image)
 
-            option.setAttribute("value", `${sign}` )
+            option.setAttribute("value", `${sign}` )       //i thought this was fun, just making my selects instead of typing them out. 
             option.textContent = `${sign}`
             select.append(option)
         }
     })
     .catch((err) => test(err))
 
-    const options1 = {
+    const options1 = {                                      //you can do only one request per fetch so i had to make several fetches requiring diffrent Keys and hosts
         method: 'GET',
         headers: {
             'X-RapidAPI-Key': '95e39c8565mshd52d9d5c85d8b80p174bd5jsn5b0ef354524f',
@@ -43,31 +43,27 @@ fetch('https://horoscope34.p.rapidapi.com/api/horoscope/signs', options)
         }
     }
 
-    const errFlag = document.getElementById("errFlag")
+    const errFlag = document.getElementById("errFlag")          //i hard coded a place for my errFlag, my results and my simple form
     const resultsCol = document.getElementById("results")
-   
-
-
     const form = document.querySelector("form")
 
     form.addEventListener("submit", (event) => {
-        event.preventDefault();
+        event.preventDefault();                             // i can hear sam now- "EVENT DOT PREVENT"
 
-        let userSign = event.target["signs"].value
-        let userDob = event.target["date"].value
+        let userSign = event.target["signs"].value          //storing the vaule of the sign the user selected so i can access it later in another fetch.
+        let userDob = event.target["date"].value         // ** I don't know if I'll have this working by the time i turn it in, but you will be able to look up the horoscope for any day once finished.
 
-        if(userSign === "???"){
+        if(userSign === "???"){                         // if you sumbit the form w/out selecting a sign an err message pop's up
             errFlag.innerHTML = "<h4><Strong>Must Enter Sign</Strong></h4>"
             errFlag.style.color = "orange"
-            form.reset()
+            form.reset()                                //then resetting the form and err flag so it doesn't stay on screen.
             setTimeout(errFlag.reset, 2000)
         }
 
-        fetch('https://horoscope34.p.rapidapi.com/api/horoscope/today', options1)
+        fetch('https://horoscope34.p.rapidapi.com/api/horoscope/today', options1)    
         .then((response) => response.json())
         .then((JSONresponse) =>{
             const results = JSONresponse.payload 
-            test(results[`${userSign}`])
             resultsCard(results[`${userSign}`])
  
         })
@@ -75,9 +71,9 @@ fetch('https://horoscope34.p.rapidapi.com/api/horoscope/signs', options)
     })
 
 
-    function resultsCard(string){
+    function resultsCard(string){                               // making cards to display my information. As i grow this project, i'd like to add another image to the card and maybe include another api that draws tarot cards for you.
 
-        const resultCard = document.createElement("article")
+        const resultCard = document.createElement("div")       //there is ingrown functionaility using classes on BOOTSTRAP, as they say im "strapped in".
         resultCard.classList.add("card")
 
         const h3Title = document.createElement("h3")
--- readme.md
+++ readme.md
@@ -1,1 +1,30 @@
-THIS IS MY READ ME WORKING PROGRESS
+Welcome to Mamma's Rice Horoscopes Page!!
+
+Welcome to Mamma's Rice Horoscopes! Once you are on the page, click the nav bar Horoscopes to move to the app page. Just put in your sign and your horoscope is revealed!!!
+
+
+This project is: 
+[x] Created a GitHub repository and upload my code to it.
+[x] GitHub repository has this readme.md file with setup instructions for my application.
+[x] this project has a coherent theme and narrative of horoscopes and pemonition~ 
+[x] project is deployed to the web through git pages. https://xavierrice.github.io/Portfolio_Project1/
+
+ [x] application has at least two .html files that are connected.
+ [x] application has one .css file that is linked to two pages.
+ [x] application has one .js file that are linked to two pages.
+ [x] makes use of CSS Grid to organize your content on the app page. *Although by using BOOTSTRAP, it creates it own 12 block grid and impliments flex.
+ [x] uses the :hover selector in an inline rule on the submit button.
+ [x] uses an HTML form to select your fire sign on the app.html page.
+ [x] have some visual changes that occur when the screen is a different size.
+
+ [x] On page load, an external API request is made. All the graphics on the app page and the selectors are loaded from the api.
+ [x] a simple form is submitted on your page,and the default behavior is prevented from taking place.
+ [x] You can "try" to incorrectly submit the form by not putting in your sign. When the form is incorrectly submitted, an error message is added to the DOM for the user.
+[x] and Based on a button click or form submission, an two external API request must be made. Upon receiving a successful response, the user is given horoscope information from one API and advice from another API.
+
+
+STRETCH GOALS:
+
+[x] i used a transition effect to make the carosle on the index page.
+[x] I used Bootstrap.
+[x] I used Trello to organzie myself https://trello.com/b/hHrMKEPo/xr-front-end-portfolio-project
